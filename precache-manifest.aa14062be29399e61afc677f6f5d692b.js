6a7d341c1d847c80a6ec204489a43262649d891e
pyll-o <paffciu@gmail.com>
2020-10-12 20:43:22 +0100

    Updates

---
+++ precache-manifest.aa14062be29399e61afc677f6f5d692b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1de74d8b2b7388bb86943e247196291",
+    "url": "/Jammming/index.html"
+  },
+  {
+    "revision": "49261c615b2ded1d942f",
+    "url": "/Jammming/static/css/main.82f1a042.chunk.css"
+  },
+  {
+    "revision": "0b6c9633a5ab4f1952a8",
+    "url": "/Jammming/static/js/2.90912fe5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Jammming/static/js/2.90912fe5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49261c615b2ded1d942f",
+    "url": "/Jammming/static/js/main.d4f3e4bd.chunk.js"
+  },
+  {
+    "revision": "fc098ea3bb94a327d1f8",
+    "url": "/Jammming/static/js/runtime-main.db4be77b.js"
+  },
+  {
+    "revision": "0b8e14235c539a31763644d00dccabd1",
+    "url": "/Jammming/static/media/background_photo_desktop.0b8e1423.jpg"
+  }
+]);
