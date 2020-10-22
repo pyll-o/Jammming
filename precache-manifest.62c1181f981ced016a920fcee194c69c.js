8ad1b888fb50020607d50f0c58775d5f0a8feb40
pyll-o <paffciu@gmail.com>
2020-10-22 19:09:29 +0100

    Updates

---
+++ precache-manifest.62c1181f981ced016a920fcee194c69c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "196a5b9dc55d751846070e671d256849",
+    "url": "/Jammming/index.html"
+  },
+  {
+    "revision": "8f0cb54a150e5ca69eef",
+    "url": "/Jammming/static/css/main.f151ba03.chunk.css"
+  },
+  {
+    "revision": "f1ed48ab1b0c34575f3d",
+    "url": "/Jammming/static/js/2.bfbfd833.chunk.js"
+  },
+  {
+    "revision": "0d6c4affe2a659999d44765f9a17cb4f",
+    "url": "/Jammming/static/js/2.bfbfd833.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f0cb54a150e5ca69eef",
+    "url": "/Jammming/static/js/main.5e981e4d.chunk.js"
+  },
+  {
+    "revision": "fc098ea3bb94a327d1f8",
+    "url": "/Jammming/static/js/runtime-main.db4be77b.js"
+  },
+  {
+    "revision": "0b8e14235c539a31763644d00dccabd1",
+    "url": "/Jammming/static/media/background_photo_desktop.0b8e1423.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3dbc8a7d01fa679e7a345236d9683fbd.js"
+  "/Jammming/precache-manifest.62c1181f981ced016a920fcee194c69c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Jammming/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
